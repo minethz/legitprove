f7398aabc56a50c4b306fa574d4e04db9082275b
Mineth Perera <149650637+minethz@users.noreply.github.com>
2025-07-12 18:19:00 +0530

    sa

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const fs = require("fs");
 const AWS = require("aws-sdk");
 const paymentsRoutes = require("./payments"); // Import payments.js
 const profileRoutes = require("./profile"); // Import profile routes
-const signupRoutes = require("./signup"); // Import signup.js
 const { exec } = require("child_process");
 
 
@@ -429,7 +428,6 @@ app.get("/reports/top-liked", async (req, res) => {
 
 app.use("/payments", paymentsRoutes); // Mount payments routes
 app.use(profileRoutes); // Add profile routes
-app.use(signupRoutes); // Mount signup routes
 
 // Start the chat server
 exec("node /Users/minethperera/Desktop/Scam_detection_System/platform-api/chatting.js", (error, stdout, stderr) => {
@@ -451,7 +449,7 @@ app.listen(port, () => {
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
+})
 
 app.get('/', (req, res) => {
   res.send('Node.js backend is working!');
